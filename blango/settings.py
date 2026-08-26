b5ca75e033bf0d5c0aab8bb505cf52007eba18a3
Prashanta Karmakar <piiyush26@gmail.com>
2026-08-26 18:33:23 +0000

    settings.py added ALLOWED HOSTS = ['.codio.io']

--- blango/settings.py
+++ blango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+sn%dpa!086+g+%44z9*^j^q-u4n!j(#wl)x9a%_1op@zz2+1-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fluteorganic-vaticancouple-8000.codio.io']
+ALLOWED_HOSTS = ['.codio.io']
 
 
 # Application definition
